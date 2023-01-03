bdf19833e1857e793d171913e1a791a9d4b81d16
Desi <mdesortes00@gmail.com>
2023-01-04 00:35:52 +0100

    minor update

--- ProjektBD/Form1.Designer.cs
+++ ProjektBD/Form1.Designer.cs
@@ -28,35 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPasswd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // loginBtn
             // 
-            this.button1.Location = new System.Drawing.Point(194, 250);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.loginBtn.Location = new System.Drawing.Point(237, 135);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(75, 23);
+            this.loginBtn.TabIndex = 0;
+            this.loginBtn.Text = "Zaloguj";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(100, 97);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLogin.TabIndex = 1;
+            // 
+            // textBoxPasswd
+            // 
+            this.textBoxPasswd.Location = new System.Drawing.Point(100, 135);
+            this.textBoxPasswd.Name = "textBoxPasswd";
+            this.textBoxPasswd.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPasswd.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hasło";
             // 
             // LoginPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 461);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPasswd);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.loginBtn);
             this.Name = "LoginPanel";
             this.Text = "Logowanie";
             this.Load += new System.EventHandler(this.LoginPanel_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPasswd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ProjektBD/Form1.cs
+++ ProjektBD/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -26,7 +27,7 @@ namespace ProjektBD
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        /*private void loginBtn_Click(object sender, EventArgs e)
         {
             MySqlConnection con = new MySqlConnection();
             con.ConnectionString = connstring;
@@ -39,6 +40,42 @@ namespace ProjektBD
                 MessageBox.Show("Miasto" + reader);
             }
 
+        }*/
+
+        string login, haslo;
+
+        private void loginBtn_Click(object sender, EventArgs e) // sprawdza czy użytkownik jest w bazie danych
+        {
+            MySqlConnection con = new MySqlConnection();
+            con.ConnectionString = connstring;
+            con.Open();
+
+            //ścieżka dostępu do bazy musisz dac swoja tutaj
+            string query = "SELECT * FROM projektbd.users WHERE login= '" + textBoxLogin.Text +
+            "'AND password = '" + textBoxPasswd.Text + " ' ";
+            MySqlCommand sda = new MySqlCommand(query, con);
+            MySqlDataReader reader = sda.ExecuteReader();
+            while (reader.Read())
+            {
+                login = reader["login"].ToString();
+                haslo = reader["password"].ToString();
+            }
+            if (login != "") // logowanie udane
+            {
+
+                    MessageBox.Show("Logowanie Udane!", "Logowanie", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    //MainPannel panel = new MainPannel();
+                    //panel.Show();
+
+            }
+            else // logowanie nieudane
+            {
+
+                    MessageBox.Show("Podane dane są nieprawidłowe.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
         }
+        
     }
 }
